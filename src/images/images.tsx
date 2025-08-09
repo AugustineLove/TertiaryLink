ee112c72187ac378e2d90e61890e8a356a5ad70e
Augustine Love Stephens <106684513+AugustineLove@users.noreply.github.com>
2025-08-09 21:20:54 +0000

    Add files via upload

---
+++ src/images/images.tsx
@@ -0,0 +1,6 @@
+import feriel from '../images/feriel.jpg';
+import bg2 from '../images/bg2.jpg';
+import bg3 from '../images/bg3.jpg';
+import bg4 from '../images/bg4.jpg';
+
+export const bgImages = [feriel, bg2, bg3, bg4];
